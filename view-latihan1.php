452c1dbf00e3dec7fb1a9f0bf0ee43ef084fccd9
salsabila000 <86759929+salsabila000@users.noreply.github.com>
2021-10-02 09:54:47 +0700

    Add files via upload
    upload tugas

---
+++ view-latihan1.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+	<title>Latihan1</title>
+</head>
+<body>
+	 <h1>Hallo kawan.. yuk kita belajar web programming <br></h1>
+	 <b>Nilai1= <?= $nilai1; ?><br>
+        Nilai2= <?= $nilai2; ?>
+    <br>ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= 
+$nilai1 . "+" . $nilai2 . "=" . $hasil; ?>		
+
+</body>
+</html>
